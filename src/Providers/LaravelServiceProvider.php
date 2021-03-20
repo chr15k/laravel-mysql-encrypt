dcee5f8a617cf8ca93189de6774d221589d540f8
Christopher <ctk8501@gmail.com>
2021-03-20 11:00:14 +0000

    fix service provider directory path

--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -15,7 +15,7 @@ class LaravelServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/config.php' => config_path('mysql-encrypt.php'),
+            __DIR__.'/../../config/config.php' => config_path('mysql-encrypt.php'),
         ], 'config');
 
         $this->addValidators();
@@ -26,6 +26,6 @@ class LaravelServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'mysql-encrypt');
+        $this->mergeConfigFrom(__DIR__.'/../../config/config.php', 'mysql-encrypt');
     }
 }
